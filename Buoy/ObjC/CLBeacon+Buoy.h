fc1ceda1fde390c0523fb306edb77af478254ca0
bennyguitar <brgordon@ua.edu>
2014-08-05 12:08:20 -0500

    CG

--- Buoy/ObjC/CLBeacon+Buoy.h
+++ Buoy/ObjC/CLBeacon+Buoy.h
@@ -20,6 +20,7 @@
 //  CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 
 #import <CoreLocation/CoreLocation.h>
+#import <CoreGraphics/CoreGraphics.h>
 #import <Foundation/Foundation.h>
 
 typedef NS_ENUM(NSInteger, kBuoyUnitType) {
